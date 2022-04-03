a8bf130992e0f564f9ef7de1b113f3c930a67c53
David Pitoniak <davidpitoniak32@gmail.com>
2022-04-03 17:19:54 -0400

    lsp: fix missing settings

--- .config/nvim/lua/user/lsp/lsp-installer.lua
+++ .config/nvim/lua/user/lsp/lsp-installer.lua
@@ -24,10 +24,5 @@ lsp_installer.on_server_ready(function(server)
     opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
   end
 
-  if server.name == "pyright" then
-    local pyright_opts = require "user.lsp.settings.pyright"
-    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-  end
-
   server:setup(opts)
 end)
