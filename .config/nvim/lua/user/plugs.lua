53fbc3514db9cf801c0446949ff31b16b328ad99
David Pitoniak <davidpitoniak32@gmail.com>
2022-04-04 19:40:44 -0400

    git integrations

--- .config/nvim/lua/user/plugs.lua
+++ .config/nvim/lua/user/plugs.lua
@@ -1,4 +1,43 @@
-local packer = require("user.packerinit")
+
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
 
 -- Install your plugins here
 return packer.startup(function(use)
@@ -13,6 +52,12 @@ return packer.startup(function(use)
     use "akinsho/toggleterm.nvim"
     use 'akinsho/bufferline.nvim'
     use "moll/vim-bbye"
+
+    use "lewis6991/gitsigns.nvim"
+    use "f-person/git-blame.nvim"
+    use "ruifm/gitlinker.nvim"
+    use "mattn/vim-gist"
+    use "mattn/webapi-vim"
 
     -- Nvim Tree
     use 'kyazdani42/nvim-tree.lua'
