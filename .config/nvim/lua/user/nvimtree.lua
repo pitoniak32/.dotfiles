78932b1cde20375472e8d5a18cca7d6cac887831
David Pitoniak <davidpitoniak32@gmail.com>
2022-04-04 19:54:09 -0400

    fix: tree icons

--- .config/nvim/lua/user/nvimtree.lua
+++ .config/nvim/lua/user/nvimtree.lua
@@ -13,8 +13,8 @@ vim.g.nvim_tree_icons = {
     ignored = "◌",
   },
   folder = {
-    -- arrow_open = " ",
-    -- arrow_closed = "",
+    arrow_open = "",
+    arrow_closed = "",
     default = "",
     open = "",
     empty = "",
