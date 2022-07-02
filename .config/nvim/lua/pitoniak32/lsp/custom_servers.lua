8aebb63ee36a05033c8cae67ed37e89d00961492
David Pitoniak <davidpitoniak32@gmail.com>
2022-07-02 11:14:38 -0400

    fix: window split bindings, lsp additions

--- .config/nvim/lua/pitoniak32/lsp/custom_servers.lua
+++ .config/nvim/lua/pitoniak32/lsp/custom_servers.lua
@@ -1,61 +1,46 @@
-local tsserver = {
-	on_attach = function(client, bufnr)
-    require("pitoniak32.lsp.default_handlers").default_attach(client, bufnr)
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
-  end,
-  init_options = {
-    preferences = {
-      importModuleSpecifierPreference = "relative"
-    },
-  },
-}
-
-local sumneko_lua = {
-	on_attach = function(client, bufnr)
-    require("pitoniak32.lsp.default_handlers").default_attach(client, bufnr)
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
-  end,
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-				-- Setup your lua path
-				path = vim.split(package.path, ";"),
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
-}
-
--- local pyright = {
--- 	on_attach = function(default_on_attach)
--- 		return function(client, bufnr)
--- 			default_on_attach(client, bufnr)
--- 		end
--- 	end,
--- }
-
--- if client.name == "html" or client.name == "jdt.ls" then
---   client.server_capabilities.document_formatting = false
--- end
-
 M = {}
 
 M.servers = {
-	tsserver = tsserver,
-	sumneko_lua = sumneko_lua,
+	tsserver = {
+    on_attach = function(client, bufnr)
+      require("pitoniak32.lsp.default_handlers").default_attach(client, bufnr)
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+    end,
+    init_options = {
+      preferences = {
+        importModuleSpecifierPreference = "relative"
+      },
+    },
+  },
+	sumneko_lua = {
+    on_attach = function(client, bufnr)
+      require("pitoniak32.lsp.default_handlers").default_attach(client, bufnr)
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+    end,
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = "LuaJIT",
+          -- Setup your lua path
+          path = vim.split(package.path, ";"),
+        },
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          library = {
+            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+            [vim.fn.stdpath("config") .. "/lua"] = true,
+          },
+        },
+      },
+    },
+  },
 	pyright = {},
+  gopls = {},   
 }
 
 return M
