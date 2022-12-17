9ed4d38e0aeb7ddd3a70eb2ebe01119ca213e488
David Pitoniak <davidpitoniak32@gmail.com>
2022-12-17 10:21:04 -0500

    feat: start using lsp-zero and simplify config

--- .config/nvim/lua/pitoniak32/plugs.lua
+++ .config/nvim/lua/pitoniak32/plugs.lua
@@ -1,103 +1,71 @@
 local fn = vim.fn
 
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
-end
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugs.lua source <afile> | PackerSync
-  augroup end
-]])
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
--- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
+local packer_bootstrap = ensure_packer()
 
--- Install your plugins here
-return packer.startup(function(use)
-  -- Necessary Plugins
-	use("wbthomason/packer.nvim")
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
 	use("nvim-lua/plenary.nvim")
 	use("nvim-telescope/telescope.nvim")
 	use({'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
 	use("ThePrimeagen/harpoon")
 	use("tpope/vim-surround")
-	use("gruvbox-community/gruvbox")
-	use("neovim/nvim-lspconfig")
-  use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-  use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/cmp-nvim-lua")
-	use("hrsh7th/nvim-cmp")
-	use("onsails/lspkind-nvim")
-	use("L3MON4D3/LuaSnip") --snippet engine
-	use("saadparwaiz1/cmp_luasnip")
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
 	})
-	use("nvim-treesitter/playground")
-	use("romgrk/nvim-treesitter-context")
-	use({ "ckipp01/stylua-nvim", run = "cargo install stylua" })
-
-  -- Quality Of Life Enhancements
-	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
 	use("moll/vim-bbye")
-	use("tpope/vim-fugitive")
-	use("numToStr/Comment.nvim")
-  use("JoosepAlviste/nvim-ts-context-commentstring")
-  use("vim-test/vim-test")
-	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
-  use("kyazdani42/nvim-tree.lua")
-  use("vimwiki/vimwiki")
-  use('mfussenegger/nvim-jdtls')
-
-  use("mfussenegger/nvim-dap")
-  use("rcarriga/nvim-dap-ui")
-  use("theHamsta/nvim-dap-virtual-text")
+	--[[ use("numToStr/Comment.nvim") ]]
+	--use("tpope/vim-fugitive")
+	--use("nvim-treesitter/playground")
+	--use("romgrk/nvim-treesitter-context")
+ --[[  use("JoosepAlviste/nvim-ts-context-commentstring") ]]
+ --[[  use("vim-test/vim-test") ]]
+	--[[ use("jose-elias-alvarez/null-ls.nvim") ]]
+ --[[  use("vimwiki/vimwiki") ]]
 
   -- Visual Enhancements
-	use("akinsho/bufferline.nvim")
-	use("lewis6991/gitsigns.nvim")
-	use("f-person/git-blame.nvim")
-	use("kyazdani42/nvim-web-devicons")
-	use("folke/tokyonight.nvim")
-	use({ "p00f/nvim-ts-rainbow" })
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+	--[[ use("lewis6991/gitsigns.nvim") ]]
+	--[[ use("f-person/git-blame.nvim") ]]
+	--[[ use("kyazdani42/nvim-web-devicons") ]]
+	--[[ use("folke/tokyonight.nvim") ]]
+	--[[ use({ "p00f/nvim-ts-rainbow" }) ]]
+  
+	-- Lsp Zero
+	use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  requires = {
+	    -- LSP Support
+	    {'neovim/nvim-lspconfig'},
+	    {'williamboman/mason.nvim'},
+	    {'williamboman/mason-lspconfig.nvim'},
 
-  -- Questionably Keeping
-	use("ray-x/lsp_signature.nvim")
-	use("RRethy/vim-illuminate")
+	    -- Autocompletion
+	    {'hrsh7th/nvim-cmp'},
+	    {'hrsh7th/cmp-buffer'},
+	    {'hrsh7th/cmp-path'},
+	    {'saadparwaiz1/cmp_luasnip'},
+	    {'hrsh7th/cmp-nvim-lsp'},
+	    {'hrsh7th/cmp-nvim-lua'},
 
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+	    -- Snippets
+	    {'L3MON4D3/LuaSnip'},
+	    {'rafamadriz/friendly-snippets'},
+	  }
+	}
+  -- End Lsp Zero
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
