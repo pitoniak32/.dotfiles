79b717505dbe7fc4d6515e808e8d72a5bb20bb39
pitoniak32 <84917393+pitoniak32@users.noreply.github.com>
2023-01-15 12:10:18 -0500

    Update install_dotfiles_fresh.sh

--- .local/bin/install_dotfiles_fresh.sh
+++ .local/bin/install_dotfiles_fresh.sh
@@ -8,9 +8,9 @@ function cfg_func {
 cfg_func checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    cfg_func checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+else
+  echo "Backing up pre-existing dot files.";
+  cfg_func checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 cfg_func checkout
 cfg_func config status.showUntrackedFiles no
