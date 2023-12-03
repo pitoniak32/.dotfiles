4ec554c9fe3041e313fed6d4d90d1270f3933ca2
David Pitoniak <davidpitoniak32@gmail.com>
2023-12-03 12:59:30 -0500

    feat: more emacs configuration

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -225,30 +225,14 @@
                  (make-local-variable 'auto-hscroll-mode)
                  (setq auto-hscroll-mode nil)))))
 
-;; (use-package lua-mode)
-;; (use-package rust-mode)
+(use-package rust-ts-mode
+  :ensure t
+  :hook ((rust-ts-mode . elgot-ensure)
+         (rust-ts-mode . company-mode))
+  :mode ("\\.rs\\'" . rust-ts-mode))
 
-(use-package lsp-mode
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (rust-mode . lsp)
-         ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
-
-;; optionally
-;; (use-package lsp-ui :commands lsp-ui-mode)
-;; if you are helm user
-;; (use-package helm-lsp :commands helm-lsp-workspace-symbol)
-;; if you are ivy user
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-;; optionally if you want to use debugger
-;; (use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+(use-package company
+  :ensure t)
 
 (use-package toc-org
     :commands toc-org-enable
