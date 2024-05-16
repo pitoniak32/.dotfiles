8c82ad7eaeb21279207ca48b1bd3e6ec649adf08
David Pitoniak <davidpitoniak32@gmail.com>
2024-05-15 20:50:29 -0400

    feat: add oil to nvim

--- .config/nvim/lua/pitoniak32/lazy.lua
+++ .config/nvim/lua/pitoniak32/lazy.lua
@@ -109,8 +109,25 @@ require("lazy").setup({
       },
     },
   },
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup {
+        columns = { "icon" },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<M-h>"] = "actions.select_split",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
 
-
+      -- Open parent directory in floating window
+      vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+    end,
+  },
   -- BEGIN Treesitter
   {
     -- Highlight, edit, and navigate code
