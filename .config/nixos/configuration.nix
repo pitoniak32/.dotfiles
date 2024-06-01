4a5c79453d4d19acda76b3bda0cbf7aaaf9c2c5e
David Pitoniak <davidpitoniak32@gmail.com>
2024-06-01 15:17:46 -0400

    feat: move nixos config into dotfiles
    Stole the rebuild script from (https://github.com/0atman) thank you!

---
+++ .config/nixos/configuration.nix
@@ -0,0 +1,192 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  boot.loader = {
+    systemd-boot.enable = false;
+    grub.enable = true;
+    grub.device = "nodev";
+    grub.efiSupport = true;
+    grub.useOSProber = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver = {
+    # Enable the X11 windowing system.
+    enable = true;
+
+    videoDrivers = ["nvidia"];
+
+    desktopManager = {
+      xterm.enable = false;
+      plasma5.enable = true;
+    };
+    #displayManager.defaultSession = "none+i3";
+    displayManager.sddm.enable = true;
+
+    # windowManager.i3 = { enable = true; package = pkgs.i3-gaps; extraPackages = with pkgs; [ dmenu i3status i3lock #i3blocks ]; };
+    
+    # Configure keymap in X11
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.davidpi = {
+    isNormalUser = true;
+    description = "davidpi";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+        zsh       # default shell
+	kitty     # terminal
+        neovim    # editor
+        stow      # symlink .dotfiles
+	brave     # browser
+        tmux
+	gnupg
+	fzf
+	jq
+	nodejs    # neovim plugins (node based)
+	gnumake   # neovim plugins (telescope-fzf)
+	gcc       # neovim plugins (telescope-fzf)
+	alejandra # rebuild script
+	# RUST PKGS
+        starship
+	eza
+	bat
+	ripgrep
+	# NET TOOLS
+	dig
+	traceroute
+	whois
+	nmap
+    ];
+  };
+
+  fonts.packages = with pkgs; [
+      pkgs.nerdfonts
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  # Install firefox.
+  programs.firefox.enable = false;
+
+  # Install zsh
+  programs.zsh.enable = true;
+
+  # Enable docker
+  virtualisation.docker.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     rustup
+     git
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     lutris
+     wineWowPackages.stable
+  #  wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
