b414b04ba67d8b7913ac4376f98117e581160bce
David Pitoniak <davidpitoniak32@gmail.com>
2024-12-05 12:53:39 -0500

    feat: add plenary dep to status

--- .config/nvim/lua/custom/plugins/statusline.lua
+++ .config/nvim/lua/custom/plugins/statusline.lua
@@ -1,6 +1,9 @@
 return {
   {
     "tjdevries/express_line.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
     config = function()
       require("custom.statusline").setup()
     end,
