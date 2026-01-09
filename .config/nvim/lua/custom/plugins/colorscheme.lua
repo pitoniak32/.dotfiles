1d98d90dbd2f61b262645fa0b4252dd4eb4a2c94
David Pitoniak <davidpitoniak32@gmail.com>
2026-01-09 15:26:03 -0500

    chore: update lazy and set colorscheme to cat macch

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -9,7 +9,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "tokyonight-storm"
+      vim.cmd.colorscheme "catppuccin-macchiato"
     end,
   },
 }
