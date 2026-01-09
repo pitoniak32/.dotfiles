64b5b4340e02b30443237b76dd621e74e1868b07
David Pitoniak <davidpitoniak32@gmail.com>
2026-01-09 15:23:17 -0500

    feat: add checksum alias

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -2,6 +2,8 @@ alias g 'git'
 alias k 'kubectl'
 alias rm 'rm -I'
 
+alias cksha256 'echo "$1" "$2" | sha256sum --check'
+
 switch $hostname
     case jawnix lemurpro d
         alias pbcopy 'xclip -selection clipboard'
