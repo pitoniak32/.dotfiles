206d3bd1d2f0db0e52d7594351cd5a0eeddaf2a4
David Pitoniak <david.pitoniak@ukg.com>
2026-03-31 13:42:40 -0400

    chore(nvim): fix treesitter versioning / config

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,22 +1,13 @@
 return {
   -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
-  build = function()
-    pcall(require("nvim-treesitter.install").update { with_sync = true })
-  end,
+  build = ":TSUpdate",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    "nvim-treesitter/playground",
-    "romgrk/nvim-treesitter-context",
+    "nvim-treesitter/nvim-treesitter-context",
   },
   config = function()
-    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-    if not status_ok then
-      print "failed to load 'nvim-treesitter.configs'"
-      return
-    end
-    ---@diagnostic disable-next-line: missing-fields
-    configs.setup {
+    require("nvim-treesitter").setup {
       ensure_installed = {
         -- programming
         "typescript",
@@ -35,29 +26,7 @@ return {
         "vimdoc",
         "hcl",
       },
-      sync_install = false,
       auto_install = false,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      autotag = {
-        enable = true,
-        disable = { "xml" },
-      },
-      rainbow = {
-        enable = true,
-        colors = {
-          "Gold",
-          "Orchid",
-          "DodgerBlue",
-          "Salmon",
-        },
-        disable = { "html" },
-      },
-      playground = {
-        enable = true,
-      },
     }
   end,
 }
