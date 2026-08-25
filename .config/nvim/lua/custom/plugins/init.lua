6391e2493ba6dd0c899a79608c680e344c53d5bf
David Pitoniak <david.pitoniak@ukg.com>
2026-08-25 08:12:09 -0400

    fix: convert comment plugin to folke

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,24 +2,9 @@ return {
   -- Not sure if this is needed
   "mfussenegger/nvim-lint",
   {
-    "numToStr/Comment.nvim",
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    config = function()
-      -- Have an issue open to see if this can get updated: https://github.com/numToStr/Comment.nvim/issues/498
-      ---@diagnostic disable-next-line: missing-fields
-      require("Comment").setup {
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      }
-    end,
-  },
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    init = function()
-      require("ts_context_commentstring").setup {
-        enable_autocmd = false,
-      }
-    end,
-  },
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+  }
 }
