ffd986f2d9b2a59de5462acf13db07c3483e1026
David Pitoniak <davidpitoniak32@gmail.com>
2026-08-29 07:38:17 -0400

    feat: switcth to hrsh7th/nvim-cmp for obsidian.nvim

--- .config/nvim/lua/custom/plugins/nvim-cmp.lua
+++ .config/nvim/lua/custom/plugins/nvim-cmp.lua
@@ -1,23 +1,47 @@
 return {
   {
-    "saghen/blink.cmp",
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
       { "L3MON4D3/LuaSnip", version = "v2.*" },
       "rafamadriz/friendly-snippets",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
     },
-    version = "1.*",
-    opts = {
-      snippets = { preset = "luasnip" },
-      keymap = { preset = "default" },
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "mono",
-      },
-      sources = {
-        default = { "lsp", "snippets", "path", "buffer" },
-      },
-      signature = { enabled = true },
-    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      local cmp = require "cmp"
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        completion = { completeopt = "menu,menuone,noinsert" },
+        mapping = cmp.mapping.preset.insert {
+          ["<C-y>"] = cmp.mapping.confirm { select = true },
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-space>"] = cmp.mapping.complete(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<CR>"] = cmp.config.disable,
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp", max_item_count = 8 },
+          { name = "luasnip", max_item_count = 8 },
+          { name = "nvim_lsp_signature_help" },
+        }, {
+          { name = "path", max_item_count = 8 },
+          { name = "buffer", max_item_count = 8 },
+        }),
+      }
+    end,
     init = function()
       local ls = require "luasnip"
 
